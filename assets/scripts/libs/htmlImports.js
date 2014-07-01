74132bb5d11dd9656c065fc421041d4c461aadb1
Powpow Shen <powpowfp@gmail.com>
2014-07-01 20:54:53 +0800

    Add html imports utility.

---
+++ assets/scripts/libs/htmlImports.js
@@ -0,0 +1,20 @@
+define([], function() {
+  'use strict';
+
+  /**
+   * htmlImports utility
+   *
+   * Write an html imports into the head of window.document.
+   * @usage: htmlImports('uri');
+   * @params {string} uri URI for the html
+   */
+  function htmlImports(uri) {
+    var head = document.querySelector('head'),
+      link = document.createElement('link');
+    link.setAttribute('rel', 'import');
+    link.setAttribute('href', uri);
+    head.appendChild(link);
+  }
+
+  return htmlImports;
+});
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,16 @@
-require(['./require.config'], function() {
+require(['config/require'], function() {
+  'use strict';
+
   require(['jquery'], function($) {
     $(document).ready(function() {
       // keep all dependencies here.
       require([
         'bootstrap',
-        'css!styles/compiled/style.css'
-      ]);
+        'css!styles/compiled/style.css',
+        'scripts/router',
+        'scripts/libs/htmlImports'
+      ], function(bootstrap, style, Router, htmlImports) {
+      });
     });
   });
 });
